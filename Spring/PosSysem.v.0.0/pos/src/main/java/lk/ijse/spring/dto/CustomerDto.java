6b9bdd4f0f748a93ed46e489fd9dcc0bcc42738d
TharinduShantha98 <tharindushantha4@gmail.com>
2022-06-20 23:14:19 -0700

    created dto and entity

--- Spring/PosSysem.v.0.0/pos/src/main/java/lk/ijse/spring/dto/CustomerDto.java
+++ Spring/PosSysem.v.0.0/pos/src/main/java/lk/ijse/spring/dto/CustomerDto.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+
 public class CustomerDto {
+    private String customerId;
+    private String firstName;
+    private String LastName;
+    private String address;
+    private String email;
+    private String telNo;
 }
--- Spring/PosSysem.v.0.0/pos/src/main/java/lk/ijse/spring/entity/Customer.java
+++ Spring/PosSysem.v.0.0/pos/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,89 @@
 package lk.ijse.spring.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    private String customerId;
+    private String firstName;
+    private String LastName;
+    private String address;
+    private String email;
+    private String telNo;
+
+
+    public Customer(String customerId, String firstName, String lastName, String address, String email, String telNo) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        LastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.telNo = telNo;
+    }
+
+    public Customer() {
+    }
+
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", telNo='" + telNo + '\'' +
+                '}';
+    }
 }
