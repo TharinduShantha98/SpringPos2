f106ffba305d69383f3f8e8b7a535ac8ecd1e046
TharinduShantha98 <tharindushantha4@gmail.com>
2022-06-20 19:28:18 -0700

    set up project

--- Spring/PosSysem.v.0.0/pos/src/main/java/lk/ijse/spring/WebInitializer.java
+++ Spring/PosSysem.v.0.0/pos/src/main/java/lk/ijse/spring/WebInitializer.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring;
 
-public class WebInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
