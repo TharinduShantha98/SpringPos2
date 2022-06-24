4e14959dc946d4780560ae6e62990fe1f77d5f75
TharinduShantha98 <tharindushantha4@gmail.com>
2022-06-23 22:52:46 -0700

    created project enviroment

--- Spring/Learning/14_fileUploader/src/main/java/lk/ijse/spring/config/JpaConfig.java
+++ Spring/Learning/14_fileUploader/src/main/java/lk/ijse/spring/config/JpaConfig.java
@@ -1,4 +1,14 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class JpaConfig {
+
 }
